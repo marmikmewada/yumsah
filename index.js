67ef305ceb8a1482ded7e6c831adbd940850635f
Marmik Mewada <95102437+marmikmewada@users.noreply.github.com>
2022-12-04 04:00:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const checkmark = document.querySelector(".pricing>div>.details>div>i:nth-child(1)");
-const infoicon = document.querySelector(".pricing>div>.details>div>i:nth-child(2)");
-checkmark.setAttribute('id', 'checkmark');
-infoicon.setAttribute('id', 'info');
+const frontcards = document.querySelectorAll(".front-card");
+const backcards = document.querySelectorAll(".back-card");
 
+const moreInfo = document.querySelectorAll(".info-div>a");
+
+moreInfo.forEach((value,index)=>{
+    moreInfo[index].addEventListener("click", function(e){
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        frontcards[index].classList.add("hidden-one");
+        backcards[index].classList.remove("hidden-one");
+        backcards[index].classList.add("hidden-two");
+    })
+})
