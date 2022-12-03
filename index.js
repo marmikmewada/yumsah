d1ca08c3b7888fb3cfadcf16c8ba91fada4af33a
Marmik Mewada <95102437+marmikmewada@users.noreply.github.com>
2022-12-04 00:30:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const checkmark = document.querySelector(".pricing>div>.details>div>i:nth-child(1)");
+const infoicon = document.querySelector(".pricing>div>.details>div>i:nth-child(2)");
+checkmark.setAttribute('id', 'checkmark');
+infoicon.setAttribute('id', 'info');
+
